006588690bf9f8f78be9ec05353a8458d72b34ad
cesarchretien <cesarchre10@gmail.com>
2017-07-26 23:33:33 +0200

    its a calculator!

--- app/src/main/java/com/devinitive/cesarchretien/firstapplication/MainActivity.kt
+++ app/src/main/java/com/devinitive/cesarchretien/firstapplication/MainActivity.kt
@@ -2,11 +2,68 @@ package com.devinitive.cesarchretien.firstapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        one.setOnClickListener {
+            calculate.append("1")
+        }
+
+        two.setOnClickListener {
+            calculate.append("2")
+        }
+
+        three.setOnClickListener {
+            calculate.append("3")
+        }
+
+        four.setOnClickListener {
+            calculate.append("4")
+        }
+
+        five.setOnClickListener {
+            calculate.append("5")
+        }
+
+        six.setOnClickListener {
+            calculate.append("6")
+        }
+
+        seven.setOnClickListener {
+            calculate.append("7")
+        }
+
+        eight.setOnClickListener {
+            calculate.append("8")
+        }
+
+        nine.setOnClickListener {
+            calculate.append("9")
+        }
+
+        zero.setOnClickListener {
+            calculate.append("0")
+        }
+
+        plus.setOnClickListener {
+            calculate.append(" + ")
+        }
+
+        minus.setOnClickListener {
+            calculate.append(" - ")
+        }
+
+        times.setOnClickListener {
+            calculate.append(" * ")
+        }
+
+        divide.setOnClickListener {
+            calculate.append(" / ")
+        }
     }
 }
